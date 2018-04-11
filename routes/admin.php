1c06801a7c8924a431108fc8e2dceb2862856796
Arryangga Aliev Pratamaputra <arryanggaputra@gmail.com>
2018-04-12 01:00:05 +0700

    Buat routing khusus webmaster/admin

--- routes/admin.php
+++ routes/admin.php
@@ -1,7 +1,12 @@
 <?php
 
 Route::group(['prefix' => '/webmaster'], function () {
-    Auth::routes();
-
-    Route::get('home', ['middleware' => ['auth', 'admin'], 'uses' => "HomeController@webmasterPage"]);
+    Route::group(['middleware' => 'guest'], function () {
+        Route::get('/login', ['uses' => 'Admin\AuthController@showLoginForm']);
+        Route::post('/login', ['uses' => 'Admin\AuthController@login']);
+    });
+    Route::group(['middleware' => ['auth', 'admin']], function () {
+        Route::get('/', ['uses' => "Admin\HomeController@index"]);
+        Route::get('/logout', ['uses' => 'Admin\AuthController@logout']);
+    });
 });
