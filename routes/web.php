a429f86edfeae999d71dac3ebb405b6b13369654
Arryangga Aliev Pratamaputra <arryanggaputra@gmail.com>
2018-04-12 00:59:39 +0700

    Pisahkan route antar member dan admin, menjadi file yang berbeda

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,8 @@
 <?php
 
 include 'admin.php';
+include 'member.php';
 
 Route::get('/', function () {
     return view('welcome');
 });
-
-Auth::routes();
-
-// Route::get('/register', 'RegisterController@create');
-// Route::post('register', 'RegisterController@store');
-
-Route::get('/home', 'HomeController@index')->name('home');
